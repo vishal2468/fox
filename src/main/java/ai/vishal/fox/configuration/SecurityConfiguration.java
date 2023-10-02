897ac17abaee8e33f830cf304a60b4df404efd0a
vishal2468 <vishalpoddar2468@gmail.com>
2023-10-02 18:09:35 +0530

    security working

--- src/main/java/ai/vishal/fox/configuration/SecurityConfiguration.java
+++ src/main/java/ai/vishal/fox/configuration/SecurityConfiguration.java
@@ -1,49 +1,30 @@
 package ai.vishal.fox.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
-    // @Autowired
-    // UserDetailsService userDetailsService;
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authz -> authz
                 .anyRequest().authenticated()
             )
-            .httpBasic(withDefaults());
+            .formLogin(withDefaults());
         return http.build();
     }
 
-    // @Override
-    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    //     auth.userDetailsService(userDetailsService);
-    // }
-
-    // @Override
-    // protected void configure(HttpSecurity http) throws Exception {
-    //     http.authorizeRequests()
-    //             .antMatchers("/admin").hasRole("ADMIN")
-    //             .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-    //             .antMatchers("/sendmessage").hasAnyRole("USER", "ADMIN")
-    //             .antMatchers("/sendmessage/*/*").hasAnyRole("USER", "ADMIN")
-    //             .antMatchers("/webhooks").permitAll()
-    //             .antMatchers("/customer").hasAnyRole("USER", "ADMIN")
-    //             .antMatchers("/customer/*").hasAnyRole("USER", "ADMIN")
-    //             .antMatchers("/").permitAll()
-    //             .and().httpBasic().and().csrf().disable();
-    // }
-    // @Bean
-    // public PasswordEncoder getPasswordEncoder() {
-    //     return NoOpPasswordEncoder.getInstance();
-    // }
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
