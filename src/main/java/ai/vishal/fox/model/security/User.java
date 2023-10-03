d6ff3379f63d4ed9754aaad1bea503b9c3c5a260
vishal2468 <vishalpoddar2468@gmail.com>
2023-10-03 06:45:42 +0530

    removed unused imports

--- src/main/java/ai/vishal/fox/model/security/User.java
+++ src/main/java/ai/vishal/fox/model/security/User.java
@@ -1,14 +1,8 @@
 package ai.vishal.fox.model.security;
 
-import java.util.UUID;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 
 @Data
