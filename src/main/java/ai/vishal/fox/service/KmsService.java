73075313ea7756370dc6c7ba137fbed82b6e4507
vishal2468 <vishalpoddar2468@gmail.com>
2023-11-01 07:18:37 +0530

    kms auth

--- src/main/java/ai/vishal/fox/service/KmsService.java
+++ src/main/java/ai/vishal/fox/service/KmsService.java
@@ -1,19 +1,16 @@
 package ai.vishal.fox.service;
 
-import java.util.Arrays;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import ai.vishal.fox.model.dto.AggregateBy;
-import ai.vishal.fox.model.dto.BucketByTime;
 import ai.vishal.fox.model.request.CipherRequest;
-import ai.vishal.fox.model.request.StatsRequestBody;
 import ai.vishal.fox.model.response.CipherResponse;
 
 @Service
@@ -22,11 +19,20 @@ public class KmsService {
     @Autowired
     RestTemplate restTemplate;
 
+    @Value("${user.devenc}")
+    private String devEnc;
+
+    @Value("${user.devdec}")
+    private String devDec;
+
+    @Value("$mek")
+    private String mek;
+    
     public String encrypt(String refreshToken) {
         refreshToken=Base64.getEncoder().encodeToString(refreshToken.getBytes());
         String url="https://KSM-endpoint";
         HttpHeaders httpHeaders=new HttpHeaders();
-        httpHeaders.setBearerAuth("Bearer token ");
+        httpHeaders.add("x-api-key", devEnc);
         CipherRequest cipherRequest=new CipherRequest(refreshToken);
         
         HttpEntity<CipherRequest> requestEntity = new HttpEntity<>(cipherRequest, httpHeaders);
@@ -36,7 +42,7 @@ public class KmsService {
     public String decrypt(String encryptedRefreshToken) {
         String url="https://KSM-endpoint";
         HttpHeaders httpHeaders=new HttpHeaders();
-        httpHeaders.setBearerAuth("Bearer token ");
+        httpHeaders.add("x-api-key", devDec);
         CipherRequest cipherRequest=new CipherRequest(encryptedRefreshToken);
         
         HttpEntity<CipherRequest> requestEntity = new HttpEntity<>(cipherRequest, httpHeaders);
